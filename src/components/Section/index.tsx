cf542d1da7ac897c1d469f06f2195a0163f46e51
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2020-08-13 08:06:43 -0300

    feature: adicionado componente section e styled component

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -1,21 +1,41 @@
 import React from 'react';
-import { Container, Content } from './styles';
+
+import {
+  Container,
+  HeaderWrapper,
+  Header,
+  DropboxLogo,
+  Content,
+} from './styles';
 
 interface Props {
   variant: 'blue' | 'beige' | 'white' | 'black';
-  title: string,
+  title: string;
   description: string;
 }
 
 const Section: React.FC<Props> = ({ variant, title, description }) => {
+  const buttonVariant = Math.round(Math.random());
+
   return (
     <Container className={variant}>
+      <HeaderWrapper>
+        <Header>
+          <h1>
+            <DropboxLogo />
+            <span>Dropbox</span>
+          </h1>
+
+          <button>{buttonVariant === 0 ? 'Interagir' : 'Acessar'}</button>
+        </Header>
+      </HeaderWrapper>
+
       <Content>
         <h2>{title}</h2>
         <p>{description}</p>
       </Content>
     </Container>
-  )
-}
+  );
+};
 
 export default Section;
